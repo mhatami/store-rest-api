68a508f111a2ec2f80c05ec98bd068b06b8335ce
Mehdi Hatami <mhatami@ent.ad.mre>
2017-08-03 01:28:15 -0400

    Added Posgress DB and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -7,7 +9,9 @@ from resources.store import Store, StoreList
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+# first one is te primary database and second one is alternative
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key='jose'
 api = Api(app)
